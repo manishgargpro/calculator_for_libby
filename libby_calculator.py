762ce60d0a5bd0cd3690eca1997a8ac60745415f
Manish Garg <manishgargpro@gmail.com>
2025-12-02 12:00:31 -0800

    fixed a bunch of things

--- libby_calculator.py
+++ libby_calculator.py
@@ -7,57 +7,78 @@ class Calculator:
         master.title("Toggle Calculator")
 
         self.total = tk.StringVar()
-        self.total.set(str(0))
+        self.total.set("0.00")
 
         self.label = tk.Label(master, textvariable=self.total, font=("Arial", 24))
         self.label.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
         # Editable Values (Top of the Code)
-        self.item_values = {
-            "Bagel": tk.StringVar(value="1.0"),
-            "Coffee": tk.StringVar(value="1.0"),
-            "Sandwich": tk.StringVar(value="2.0"),
-            "Juice": tk.StringVar(value="3.0")
-        }
+        self.item_values = [
+            {
+                "item_name": "Bagel",
+                "item_price": 1,
+                "check_var": tk.BooleanVar()
+            },
+            {
+                "item_name": "Coffee",
+                "item_price": 2,
+                "check_var": tk.BooleanVar()
+            },
+            {
+                "item_name": "Sandwich",
+                "item_price": 3,
+                "check_var": tk.BooleanVar()
+            },
+            {
+                "item_name": "Juice",
+                "item_price": 4,
+                "check_var": tk.BooleanVar()
+            }
+        ]
 
         # Checkbuttons
-        for i in self.item_values:
-            counter += 1
-            self.button_check = ttk.Checkbutton(master, text=i, variable=self.item_values["Bagel"], command=lambda: self.toggle_item(self.item_values["Bagel"].get()))
-        self.bagel_check.grid(row=1, column=counter-1, padx=5, pady=5)
-        # self.bagel_check = ttk.Checkbutton(master, text="Bagel", variable=self.item_values["Bagel"], command=lambda: self.toggle_item(self.item_values["Bagel"].get()))
-        # self.bagel_check.grid(row=1, column=0, padx=5, pady=5)
-
-        # self.coffee_check = ttk.Checkbutton(master, text="Coffee", variable=self.item_values["Coffee"], command=lambda: self.toggle_item(self.item_values["Coffee"].get()))
-        # self.coffee_check.grid(row=1, column=1, padx=5, pady=5)
-
-        # self.sandwich_check = ttk.Checkbutton(master, text="Sandwich", variable=self.item_values["Sandwich"], command=lambda: self.toggle_item(self.item_values["Sandwich"].get()))
-        # self.sandwich_check.grid(row=1, column=2, padx=5, pady=5)
-
-        # self.juice_check = ttk.Checkbutton(master, text="Juice", variable=self.item_values["Juice"], command=lambda: self.toggle_item(self.item_values["Juice"].get()))
-        # self.juice_check.grid(row=1, column=3, padx=5, pady=5)
+        for i, item in enumerate(self.item_values):
+            self.check_button = ttk.Checkbutton(master, style="Toolbutton", text=item["item_name"], variable=item["check_var"], command=lambda item=item: self.toggle_item(item))
+            self.check_button.grid(row=1, column=i, padx=5, pady=5)
 
         # Add Tax Button
-        self.add_tax_button = ttk.Button(master, text="Add Tax (10%)", command=lambda: self.add_tax(0.10))
+        self.tax_button_bool = tk.BooleanVar()
+        self.add_tax_button = ttk.Checkbutton(master, style="Toolbutton", text="Add Tax (10%)", variable=self.tax_button_bool, command=lambda: self.add_tax(0.10))
         self.add_tax_button.grid(row=2, column=0, columnspan=4, padx=10, pady=10)
 
-    def toggle_item(self, amount):
+        # Add Clear Button
+        self.add_clear_button = ttk.Button(master, style="Toolbutton", text="Clear", command=lambda: self.total.set("0.00"))
+        self.add_clear_button.grid(row=2, column=1, columnspan=4, padx=10, pady=10)
+
+    def toggle_item(self, item):
         try:
+            print(item["item_name"], item["item_price"], item["check_var"].get())
             if self.total.get() != "0":
-                new_total = float(self.total.get()) + float(amount)
-                self.total.set(str(new_total))
+                if item["check_var"].get():
+                    new_total = float(self.total.get()) + item["item_price"]
+                else:
+                    new_total = float(self.total.get()) - item["item_price"]
+                self.total.set(f'{new_total:.2f}')
             else:
-                self.total.set(str(amount))
+                self.total.set(f'{item["item_price"]:.2f}')
         except ValueError:
-            self.total.set("0")
+            print("ValueError")
+            self.total.set("0.00")
 
     def add_tax(self, tax_rate):
         try:
+            print(self.tax_button_bool.get())
             amount = float(self.total.get())
             tax_amount = amount * tax_rate
-            self.total.set(str(amount + tax_amount))
+            if self.tax_button_bool.get():
+                print(amount, tax_amount)
+                self.total.set(f'{amount + tax_amount:.2f}')
+            else:
+                print(amount, tax_amount)
+                self.total.set(f'{amount - tax_amount:.2f}')
         except ValueError:
-            self.total.set("0")
+            print("ValueError")
+            self.total.set("0.00")
 
 root = tk.Tk()
 calculator = Calculator(root)
